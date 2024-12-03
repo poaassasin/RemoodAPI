e2668bcefb5e20423ac40574bbd8ec8820f7f3b7
poaassasin <azkaujicoba@gmail.com>
2024-12-03 20:16:31 +0700

    Menambah Dialog Delete di Fragment Jurnal

--- app/src/main/java/com/example/remood/JurnalFragment.java
+++ app/src/main/java/com/example/remood/JurnalFragment.java
@@ -1,6 +1,9 @@
 package com.example.remood;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,9 +14,14 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.example.remood.databinding.FragmentJurnalBinding;
 import com.example.remood.model.JurnalModel;
@@ -122,22 +130,7 @@ public class JurnalFragment extends Fragment implements JurnalItemAdapter.ItemCa
         JurnalDAO jurnalDAO = jurnalDB.getJurnalDAO();
         switch (pilihan) {
             case "delete" :
-                try {
-                    String waktuDiTarik = listData.get(position).getWaktu();
-                    Log.d("JurnalFragment", "Deleted item with ID: "+waktuDiTarik);
-                    jurnalDAO.deleteJurnal(jurnalDB.getJurnalDAO().findByWaktu(waktuDiTarik));
-                    listData.remove(position);
-                    List<JurnalModel> listJurnal = jurnalDB.getJurnalDAO().getAllJurnal();
-                    if (listJurnal.isEmpty()) {
-                        binding.rvList.setVisibility(View.INVISIBLE);
-                        binding.placeholderContainer.setVisibility(View.VISIBLE);
-                    }
-                }catch (Exception e) {
-                    Log.d("JurnalFragment", e.getMessage());
-                    Toast.makeText(this.getActivity(), "Tidak Berhasil", Integer.parseInt(e.getMessage()));
-                }
-                adapter.notifyDataSetChanged();
-                adapter.notifyItemRemoved(position);
+                showDialogHapus(position);
                 break;
             case "edit":
                 Intent intent = new Intent(getActivity().getApplicationContext(), EditJurnal.class);
@@ -152,6 +145,70 @@ public class JurnalFragment extends Fragment implements JurnalItemAdapter.ItemCa
             default:
                 Toast.makeText(getActivity().getApplicationContext(), "Tidak ada pilihan", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void showDialogHapus(int position) {
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_delete);
+
+        Button btHapus = dialog.findViewById(R.id.btnHapusDialog);
+        Button btCancel = dialog.findViewById(R.id.btnCancelDialog);
+        ImageView ivEmoji = dialog.findViewById(R.id.emosiDialog);
+        TextView tvDate = dialog.findViewById(R.id.dialogDate);
+        TextView tvJudul = dialog.findViewById(R.id.dialogJudul);
+        TextView tvDeskripsi = dialog.findViewById(R.id.dialogDeskripsi);
+        jurnalDB = Room.databaseBuilder(getActivity().getApplicationContext(), JurnalDatabase.class,
+                "JurnalDB").allowMainThreadQueries().build();
+        JurnalDAO jurnalDAO = jurnalDB.getJurnalDAO();
+        int emotionResource = 0;
+        switch (listData.get(position).getLinkGambarEmosi()) {
+            case "Marah!":
+                emotionResource = R.drawable.perasaanmarah;
+                break;
+            case "Happy!":
+                emotionResource = R.drawable.perasaansenang;
+                break;
+            case "Meh":
+                emotionResource = R.drawable.perasaandatar;
+                break;
+            case "Oke!":
+                emotionResource = R.drawable.perasaanoke;
+                break;
+            case "Sedih:(":
+                emotionResource = R.drawable.perasaansedih;
+                break;
+            default:
+                emotionResource = R.drawable.image_perasaan;
+        }
+        ivEmoji.setImageResource(emotionResource);
+        tvDate.setText(listData.get(position).getTanggal() + " | " + listData.get(position).getWaktu());
+        tvJudul.setText(listData.get(position).getCurhatan());
+        tvDeskripsi.setText(listData.get(position).getDetailCurhatan());
+
+        btHapus.setOnClickListener(v -> {
+            try {
+                String waktuDiTarik = listData.get(position).getWaktu();
+                Log.d("JurnalFragment", "Deleted item with ID: "+waktuDiTarik);
+                jurnalDAO.deleteJurnal(jurnalDB.getJurnalDAO().findByWaktu(waktuDiTarik));
+                listData.remove(position);
+                List<JurnalModel> listJurnal = jurnalDB.getJurnalDAO().getAllJurnal();
+                if (listJurnal.isEmpty()) {
+                    binding.rvList.setVisibility(View.INVISIBLE);
+                    binding.placeholderContainer.setVisibility(View.VISIBLE);
+                }
+            }catch (Exception e) {
+                Log.d("JurnalFragment", e.getMessage());
+                Toast.makeText(getActivity(), "Tidak Berhasil", Integer.parseInt(e.getMessage()));
+            }
+            adapter.notifyDataSetChanged();
+            adapter.notifyItemRemoved(position);
+            dialog.dismiss();
+        });
+        btCancel.setOnClickListener(v -> dialog.dismiss());
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
     }
 
